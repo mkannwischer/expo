jadephilipoom/opentitan

[crypto] mlkem-native integration
This commit adds an integration of mlkem-native into OpenTitan.
mlkem-native is a high-speed high-assurance C90 implementation of
ML-KEM (FIPS203); mlkem-native also has native code for AArch64 and
x86_64, but that is unrelated to this integration.

All C code in mlkem-native has been proved to be memory-safe (no buffer
overflow), and type-safe (no integer overflow) using the C Bounded
Model Checker (CBMC).

C code is hardened against compiler-introduced timing side-channels
(such as KyberSlash or clangover) through suitable barriers and
constant-time patterns.

For more information, see
https://github.com/pq-code-package/mlkem-native

Integration is achieved through a bazel dependency using mlkem-native's
multi-level monobuild
(https://github.com/pq-code-package/mlkem-native/tree/main/examples/monolithic_build_multilevel).
A wrapper using OT cryptolib conventions is added.
Tests are added comparing against known test vectors for the
deterministic (derand) APIs, and comparing the derived shared secrets
for the randomized APIs.

The most noteworthy aspect of this integration is the use of
OpenTitan's KMAC unit which results in a massive performance boost.
It required a small change to mlkem-native: Previously, during
rejection sampling, mlkem-native was operating on multiple Keccak
states in parallel.
While this is possible when the state lives in memory (and beneficial
on platforms that benefit from batched Keccak computations, e.g., most
CPUs with vector instructions), it is incompatible with using a Keccak
accelerator such as OT's KMAC where only a single state lives inside
of the accelerator.
To enable mlkem-native on OT, a configuration option
MLK_CONFIG_SERIAL_FIPS202_ONLY was added to mlkem-native that disables
the batching for the rejection sampling.
See https://github.com/pq-code-package/mlkem-native/pull/1231

Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

sw/device/lib/crypto/impl/mlkem/config.h
@@ -1,0 +1,105 @@
+// Copyright The mlkem-native project authors
+// Copyright zeroRISC Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MLK_CONFIG_H
+#define MLK_CONFIG_H
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_NAMESPACE_PREFIX
+ *
+ * Description: The prefix to use to namespace global symbols from mlkem/.
+ *
+ *              In a multi-level build (that is, if either
+ *              - MLK_CONFIG_MULTILEVEL_WITH_SHARED, or
+ *              - MLK_CONFIG_MULTILEVEL_NO_SHARED,
+ *              are set, level-dependent symbols will additionally be prefixed
+ *              with the parameter set (512/768/1024).
+ *
+ *              This can also be set using CFLAGS.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_NAMESPACE_PREFIX mlkem
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_NO_RANDOMIZED_API
+ *
+ * Description: If this option is set, mlkem-native will be built without the
+ *              randomized API functions (crypto_kem_keypair and
+ *              crypto_kem_enc).
+ *.             This allows users to build mlkem-native without providing a
+ *              randombytes() implementation if they only need the
+ *              deterministic API
+ *              (crypto_kem_keypair_derand, crypto_kem_enc_derand,
+ *              crypto_kem_dec).
+ *
+ *              NOTE: This option is incompatible with MLK_CONFIG_KEYGEN_PCT
+ *              as the current PCT implementation requires crypto_kem_enc().
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_NO_RANDOMIZED_API
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_INTERNAL_API_QUALIFIER
+ *
+ * Description: If set, this option provides an additional function
+ *              qualifier to be added to declarations of internal API.
+ *
+ *              The primary use case for this option are single-CU builds,
+ *              in which case this option can be set to `static`.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_INTERNAL_API_QUALIFIER static
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_FIPS202_CUSTOM_HEADER
+ *
+ * Description: Custom header to use for FIPS-202
+ *
+ *              This should only be set if you intend to use a custom
+ *              FIPS-202 implementation, different from the one shipped
+ *              with mlkem-native.
+ *
+ *              If set, it must be the name of a file serving as the
+ *              replacement for mlkem/fips202/fips202.h, and exposing
+ *              the same API (see FIPS202.md).
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_FIPS202_CUSTOM_HEADER "fips202_glue.h"
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_FIPS202X4_CUSTOM_HEADER
+ *
+ * Description: Custom header to use for FIPS-202-X4
+ *
+ *              This should only be set if you intend to use a custom
+ *              FIPS-202 implementation, different from the one shipped
+ *              with mlkem-native.
+ *
+ *              If set, it must be the name of a file serving as the
+ *              replacement for mlkem/fips202/fips202x4.h, and exposing
+ *              the same API (see FIPS202.md).
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_FIPS202X4_CUSTOM_HEADER "fips202x4_glue.h"
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_SERIAL_FIPS202_ONLY
+ *
+ * Description: If this option is set, batched Keccak operations will be
+ *              disabled for rejection sampling during matrix generation.
+ *              Instead, matrix entries will be generated one at a time.
+ *
+ *              This allows offloading Keccak computations to a hardware
+ *              accelerator that holds only a single Keccak state locally,
+ *              rather than requiring support for batched (4x) Keccak states.
+ *
+ *              NOTE: Depending on the target CPU, disabling batched Keccak
+ *              may reduce performance when using software FIPS202
+ *              implementations. Only enable this when you have to.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_SERIAL_FIPS202_ONLY
+
+#endif /* !MLK_CONFIG_H */

sw/device/lib/crypto/impl/mlkem/fips202_glue.h
@@ -1,0 +1,77 @@
+// Copyright The mlkem-native project authors
+// Copyright zeroRISC Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MLK_FIPS202_FIPS202_H
+#define MLK_FIPS202_FIPS202_H
+#include <stdint.h>
+
+#include "mlkem/src/common.h"
+#include "sw/device/lib/crypto/drivers/kmac.h"
+#include "sw/device/lib/crypto/include/sha3.h"
+
+#define SHAKE128_RATE 168
+
+typedef struct {
+} MLK_ALIGN mlk_shake128ctx;
+
+static MLK_INLINE void mlk_shake128_absorb_once(mlk_shake128ctx *state,
+                                                const uint8_t *input,
+                                                size_t inlen) {
+  (void)state;
+  kmac_shake128_start();
+  kmac_absorb(input, inlen);
+  kmac_process();
+}
+
+static MLK_INLINE void mlk_shake128_squeezeblocks(uint8_t *output,
+                                                  size_t nblocks,
+                                                  mlk_shake128ctx *state) {
+  (void)state;
+  kmac_squeeze_blocks(nblocks, kHardenedBoolFalse, (uint32_t *)output, NULL);
+}
+
+static MLK_INLINE void mlk_shake128_init(mlk_shake128ctx *state) {
+  (void)state;
+}
+
+static MLK_INLINE void mlk_shake128_release(mlk_shake128ctx *state) {
+  (void)state;
+  kmac_squeeze_end(0, kHardenedBoolFalse, NULL, NULL);
+}
+
+static MLK_INLINE void mlk_shake256(uint8_t *output, size_t outlen,
+                                    const uint8_t *input, size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = outlen / 4,
+                               .mode = kOtcryptoHashXofModeShake256};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_shake256(d, &md);
+}
+
+static MLK_INLINE void mlk_sha3_256(uint8_t *output, const uint8_t *input,
+                                    size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = 32 / 4,
+                               .mode = kOtcryptoHashModeSha3_256};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_sha3_256(d, &md);
+}
+
+static MLK_INLINE void mlk_sha3_512(uint8_t *output, const uint8_t *input,
+                                    size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = 64 / 4,
+                               .mode = kOtcryptoHashModeSha3_512};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_sha3_512(d, &md);
+}
+
+#endif /* MLK_FIPS202_FIPS202_H */

mkannwischer/expo

Signed-off-by: Jade Philipoom <jadep@zerorisc.com>
Co-authored-by: Jade Philipoom <jadep@zerorisc.com>

sw/device/lib/crypto/impl/mlkem/config.h
@@ -1,0 +1,105 @@
+// Copyright The mlkem-native project authors
+// Copyright zeroRISC Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_CONFIG_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_CONFIG_H_
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_NAMESPACE_PREFIX
+ *
+ * Description: The prefix to use to namespace global symbols from mlkem/.
+ *
+ *              In a multi-level build (that is, if either
+ *              - MLK_CONFIG_MULTILEVEL_WITH_SHARED, or
+ *              - MLK_CONFIG_MULTILEVEL_NO_SHARED,
+ *              are set, level-dependent symbols will additionally be prefixed
+ *              with the parameter set (512/768/1024).
+ *
+ *              This can also be set using CFLAGS.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_NAMESPACE_PREFIX mlkem
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_NO_RANDOMIZED_API
+ *
+ * Description: If this option is set, mlkem-native will be built without the
+ *              randomized API functions (crypto_kem_keypair and
+ *              crypto_kem_enc).
+ *.             This allows users to build mlkem-native without providing a
+ *              randombytes() implementation if they only need the
+ *              deterministic API
+ *              (crypto_kem_keypair_derand, crypto_kem_enc_derand,
+ *              crypto_kem_dec).
+ *
+ *              NOTE: This option is incompatible with MLK_CONFIG_KEYGEN_PCT
+ *              as the current PCT implementation requires crypto_kem_enc().
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_NO_RANDOMIZED_API
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_INTERNAL_API_QUALIFIER
+ *
+ * Description: If set, this option provides an additional function
+ *              qualifier to be added to declarations of internal API.
+ *
+ *              The primary use case for this option are single-CU builds,
+ *              in which case this option can be set to `static`.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_INTERNAL_API_QUALIFIER static
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_FIPS202_CUSTOM_HEADER
+ *
+ * Description: Custom header to use for FIPS-202
+ *
+ *              This should only be set if you intend to use a custom
+ *              FIPS-202 implementation, different from the one shipped
+ *              with mlkem-native.
+ *
+ *              If set, it must be the name of a file serving as the
+ *              replacement for mlkem/fips202/fips202.h, and exposing
+ *              the same API (see FIPS202.md).
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_FIPS202_CUSTOM_HEADER "fips202_glue.h"
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_FIPS202X4_CUSTOM_HEADER
+ *
+ * Description: Custom header to use for FIPS-202-X4
+ *
+ *              This should only be set if you intend to use a custom
+ *              FIPS-202 implementation, different from the one shipped
+ *              with mlkem-native.
+ *
+ *              If set, it must be the name of a file serving as the
+ *              replacement for mlkem/fips202/fips202x4.h, and exposing
+ *              the same API (see FIPS202.md).
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_FIPS202X4_CUSTOM_HEADER "fips202x4_glue.h"
+
+/******************************************************************************
+ * Name:        MLK_CONFIG_SERIAL_FIPS202_ONLY
+ *
+ * Description: If this option is set, batched Keccak operations will be
+ *              disabled for rejection sampling during matrix generation.
+ *              Instead, matrix entries will be generated one at a time.
+ *
+ *              This allows offloading Keccak computations to a hardware
+ *              accelerator that holds only a single Keccak state locally,
+ *              rather than requiring support for batched (4x) Keccak states.
+ *
+ *              NOTE: Depending on the target CPU, disabling batched Keccak
+ *              may reduce performance when using software FIPS202
+ *              implementations. Only enable this when you have to.
+ *
+ *****************************************************************************/
+#define MLK_CONFIG_SERIAL_FIPS202_ONLY
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_CONFIG_H_

sw/device/lib/crypto/impl/mlkem/fips202_glue.h
@@ -1,0 +1,77 @@
+// Copyright The mlkem-native project authors
+// Copyright zeroRISC Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_FIPS202_GLUE_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_FIPS202_GLUE_H_
+#include <stdint.h>
+
+#include "mlkem/src/common.h"
+#include "sw/device/lib/crypto/drivers/kmac.h"
+#include "sw/device/lib/crypto/include/sha3.h"
+
+#define SHAKE128_RATE 168
+
+typedef struct {
+} MLK_ALIGN mlk_shake128ctx;
+
+static MLK_INLINE void mlk_shake128_absorb_once(mlk_shake128ctx *state,
+                                                const uint8_t *input,
+                                                size_t inlen) {
+  (void)state;
+  kmac_shake128_begin();
+  kmac_absorb(input, inlen);
+  kmac_process();
+}
+
+static MLK_INLINE void mlk_shake128_squeezeblocks(uint8_t *output,
+                                                  size_t nblocks,
+                                                  mlk_shake128ctx *state) {
+  (void)state;
+  kmac_squeeze_blocks(nblocks, kHardenedBoolFalse, (uint32_t *)output, NULL);
+}
+
+static MLK_INLINE void mlk_shake128_init(mlk_shake128ctx *state) {
+  (void)state;
+}
+
+static MLK_INLINE void mlk_shake128_release(mlk_shake128ctx *state) {
+  (void)state;
+  kmac_squeeze_end(0, kHardenedBoolFalse, NULL, NULL);
+}
+
+static MLK_INLINE void mlk_shake256(uint8_t *output, size_t outlen,
+                                    const uint8_t *input, size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = outlen / 4,
+                               .mode = kOtcryptoHashXofModeShake256};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_shake256(d, &md);
+}
+
+static MLK_INLINE void mlk_sha3_256(uint8_t *output, const uint8_t *input,
+                                    size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = 32 / 4,
+                               .mode = kOtcryptoHashModeSha3_256};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_sha3_256(d, &md);
+}
+
+static MLK_INLINE void mlk_sha3_512(uint8_t *output, const uint8_t *input,
+                                    size_t inlen) {
+  otcrypto_hash_digest_t md = {.data = (uint32_t *)output,
+                               .len = 64 / 4,
+                               .mode = kOtcryptoHashModeSha3_512};
+
+  otcrypto_const_byte_buf_t d = {.data = input, .len = inlen};
+
+  otcrypto_sha3_512(d, &md);
+}
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_IMPL_MLKEM_FIPS202_GLUE_H_